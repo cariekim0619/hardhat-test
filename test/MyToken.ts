a2a62d4f12c01b7547d1cb8e7b6028249a515b28
cariekim0619 <cariekim0619@hanyang.ac.kr>
2025-11-11 21:02:47 +0900

    constant renaming

--- test/MyToken.ts
+++ test/MyToken.ts
@@ -3,8 +3,8 @@ import { expect } from "chai"
 import { MyToken } from "../typechain-types"
 import { HardhatEthersSigner } from "@nomicfoundation/hardhat-ethers/signers"
 
-const mintingAmount = 100n;
-const decimals = 18n;
+const MINTINGAMOUNT = 100n;
+const DECIMALS = 18n;
 
 describe("mytoken deploy", () => {
     let myTokenC:MyToken;
@@ -15,8 +15,8 @@ describe("mytoken deploy", () => {
         myTokenC = await hre.ethers.deployContract("MyToken", [
             "MyToken",
             "MT",
-            decimals,
-            mintingAmount,
+            DECIMALS,
+            MINTINGAMOUNT,
         ]);
     });
 
@@ -29,19 +29,19 @@ describe("mytoken deploy", () => {
             expect(await myTokenC.symbol()).equal("MT");
         });
 
-        it("should return decimals", async () => {
-            expect(await myTokenC.decimals()).equal(decimals);
+        it("should return DECIMALS", async () => {
+            expect(await myTokenC.decimals()).equal(DECIMALS);
         });
 
         it("should return 0 totalSupply", async () => {
-            expect(await myTokenC.totalSupply()).equal(mintingAmount * 10n ** decimals);
+            expect(await myTokenC.totalSupply()).equal(MINTINGAMOUNT * 10n ** DECIMALS);
         });
     })
 
     describe("Mint", () => {
         it("should return 1MT balance for signer 0", async () => {
             const signer0 = signers[0];
-            expect(await myTokenC.balanceOf(signer0)).equal(mintingAmount* 10n ** decimals);
+            expect(await myTokenC.balanceOf(signer0)).equal(MINTINGAMOUNT* 10n ** DECIMALS);
         });
     })
 
@@ -49,11 +49,12 @@ describe("mytoken deploy", () => {
         it("should have 0.5MT", async () => {
             const signer0 = signers[0];
             const signer1 = signers[1];
-            await expect(myTokenC.transfer(hre.ethers.parseUnits("0.5", decimals), signer1.address)).to.emit(myTokenC, "Transfer").withArgs(signer0.address, signer1.address, hre.ethers.parseUnits("0.4", decimals));
+
+            await expect(myTokenC.transfer(hre.ethers.parseUnits("0.5", DECIMALS), signer1.address)).to.emit(myTokenC, "Transfer").withArgs(signer0.address, signer1.address, hre.ethers.parseUnits("0.5", DECIMALS));
             /* const receipt = await tx.wait();
             console.log(receipt?.logs); */
 
-            expect(await myTokenC.balanceOf(signer1.address)).equal(hre.ethers.parseUnits("0.5", decimals));
+            expect(await myTokenC.balanceOf(signer1.address)).equal(hre.ethers.parseUnits("0.5", DECIMALS));
 
             /* const filter = myTokenC.filters.Transfer(signer0.address);
             const logs = await myTokenC.queryFilter(filter, 0, "latest");
@@ -66,9 +67,21 @@ describe("mytoken deploy", () => {
 
         it ("should be reverted with insufficient balance error", async () => {
             const signer1 = signers[1];
-            await expect(myTokenC.transfer(hre.ethers.parseUnits((mintingAmount + 1n).toString(), decimals), signer1.address)).to.be.revertedWith("insufficient balance");
+            await expect(myTokenC.transfer(hre.ethers.parseUnits((MINTINGAMOUNT + 1n).toString(), DECIMALS), signer1.address)).to.be.revertedWith("insufficient balance");
         });
-    })
+    });
 
-    
+    describe("TransferFrom", () => {
+        it("should emit Approval event", async () => {
+            const signer1 = signers[1];
+            await expect(myTokenC.approve(signer1.address, hre.ethers.parseUnits("10", DECIMALS))).to.emit(myTokenC, "Approval").withArgs(signer1.address, hre.ethers.parseUnits("10", DECIMALS));
+        });
+
+        it("should be reverted with insufficient allowance error", async () => {
+            const signer0 = signers[0];
+            const signer1 = signers[1];
+
+            await expect(myTokenC.connect(signer1).transferFrom(signer0.address, signer1.address, hre.ethers.parseUnits("1", DECIMALS))).to.be.revertedWith("insufficient allowance");
+        });
+    });
 });
