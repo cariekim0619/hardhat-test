8682aa803cce6a9a572b7d9ceaf4c080cbeb3772
cariekim0619 <cariekim0619@hanyang.ac.kr>
2025-11-11 23:02:05 +0900

    add reward logic

--- contracts/TinyBank.sol
+++ contracts/TinyBank.sol
@@ -7,20 +7,36 @@
 // - users token management
 // - user → deposit → TinyBank → transfer(user → TinyBank)
 
+// Reward
+// - reward token : MyToken
+// - reward resources : 1 MT/block minting
+// - reward strategy: staked luserl/totalStaked distribution
+
+// - signer block 0 staking
+// - signer block 5 staking
+// - 0-- 1-- 2-- 3-- 4-- 5--
+//   |                   |
+// - signer0 10MT        signer1 10MT
+
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.28;
 
 interface IMyToken {
     function transfer(uint256 amount, address to) external;
-
     function transferFrom(address from, address to, uint256 amount) external;
+    function mint(uint256 amount, address owner) external;
 }
 
 contract TinyBank {
-    event Staked (address, uint256);
+    event Staked (address from, uint256 amount);
+    event Withdraw (uint256 amount, address to);
 
     IMyToken public stakingToken;
+
+    mapping (address => uint256) public lastClaimedBlock;
+    uint256 rewardPerBlock = 1 * 10 ** 18;
+
     mapping (address => uint256) public staked;
     uint256 public totalStaked;
 
@@ -28,11 +44,31 @@ contract TinyBank {
         stakingToken = _stakingToken;
     }
 
+    // who, when?
+    function distributeReward(address to) internal {
+        if (totalStaked == 0) return; // Error: VM Exception while processing transaction: reverted with panic code 0x12 (Division or modulo division by zero)
+
+        uint256 blocks = block.number - lastClaimedBlock[to];
+        uint256 reward = (blocks * rewardPerBlock * staked[to]) / totalStaked;
+        stakingToken.mint(reward, to);
+        lastClaimedBlock[to] = block.number;
+    }
+
     function stake(uint256 _amount) external {
-        require (_amount >= 0, "cannot stake 0 amount");
+        require(_amount >= 0, "cannot stake 0 amount");
+        distributeReward(msg.sender);
         stakingToken.transferFrom(msg.sender, address(this), _amount);
-        staked [msg. sender] += _amount;
+        staked[msg.sender] += _amount;
         totalStaked += _amount;
         emit Staked (msg.sender, _amount);
+    }
+
+    function withdraw(uint256 _amount) external {
+        require(staked[msg.sender] >= _amount, "insufficient staked token");
+        distributeReward(msg.sender);
+        stakingToken.transfer(_amount, msg.sender);
+        staked[msg.sender] -= _amount;
+        totalStaked -= _amount;
+        emit Withdraw(_amount, msg.sender);
     }
 }
